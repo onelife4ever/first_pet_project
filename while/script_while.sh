0d7d0db91aebc36e98e9c8828d3a1be11aec6816
onelife4ever <onelife4ever@yandex.ru>
2023-11-16 17:01:18 +0300

    note

---
+++ while/script_while.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+num=0
+
+while [ $num -le 9 ]
+	do echo $num
+		num=$(expr $num + 1)
+done
+
+i=0
+while [ $i -le 10 ]
+	do echo "Hello world"
+		i=$(expr $i + 1)
+done
+
+read -p "Введите название текстового документа, который нужно пронумеровать: " file
+numb=1
+case $file in
+$file) while read line
+		do echo "Line $numb: $line"
+			numb=$(expr $numb + 1)
+	done;;
+*) echo "Такого файла не существует"
+esac
